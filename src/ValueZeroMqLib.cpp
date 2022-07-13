ec53132638f251c84cf861ea259aa4c864665a39
benzwarpig <550791785@qq.com>
2022-07-14 00:04:46 +0800

    🐞 fix(ip): subscribe ip error

--- src/ValueZeroMqLib.cpp
+++ src/ValueZeroMqLib.cpp
@@ -10,14 +10,17 @@ int main( void )
 
     ZeroMqFactory::ValueZeroMqPublish::GetInstance().RegisterPublisher< LSLAM::MyOccupancyGrid >( "tcp://*:1234" );
 
-    ZeroMqFactory::ValueZeroMqSubscribe::GetInstance().RegisterPublisher< LSLAM::MyOccupancyGrid >( "tcp://*:1234",
+    ZeroMqFactory::ValueZeroMqSubscribe::GetInstance().RegisterPublisher< LSLAM::MyOccupancyGrid >( "tcp://localhost:1234",
                                                                                                     []( const LSLAM::MyOccupancyGrid& msg ) {
-                                                                                                        spdlog::info( "Subscribe once" );
+                                                                                                        spdlog::info( "Subscribe once {} {}", msg.saved_origin_x_(), msg.saved_origin_y_() );
                                                                                                     } );
-
     while ( 1 )
     {
         LSLAM::MyOccupancyGrid msg;
+
+        msg.set_saved_origin_x_( 1000 );
+        msg.set_saved_origin_y_( 500 );
+
         ZeroMqFactory::ValueZeroMqPublish::GetInstance().PublishProtoMsg< LSLAM::MyOccupancyGrid >( msg );
 
         std::this_thread::sleep_for( std::chrono::milliseconds( 1000 ) );
