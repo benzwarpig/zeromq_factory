2b82483027836d15de2a103b4ecd446c2d1d989f
benzs_war_pig <550791785@qq.com>
2022-07-14 16:02:29 +0800

    🔧 build(cmake): 新增cmake调用脚本代码
    cmake调用脚本实现自动化编译proto文件

--- proto/run_me.sh
+++ proto/run_me.sh
@@ -1,12 +1,15 @@
+DIR=$(cd $(dirname $0) && pwd )
+echo  $DIR
+
 #创建目录
-file_path="./proto"
-if [[ ! -d "$file_path" ]]; then
+file_path="$DIR/proto"
+if [ ! -d "$file_path" ]; then
  echo "mkdir $file_path"
  mkdir "$file_path"
 fi
 
-proto_source_path="./proto_source"
-if [[ ! -d "$proto_source_path" ]]; then
+proto_source_path="$DIR/proto_source"
+if [ ! -d "$proto_source_path" ]; then
  echo "mkdir $proto_source_path"
  mkdir "$proto_source_path"
 fi
